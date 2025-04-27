09500a737e09e03b33c58fa5ed1831f3b5a3bc35
khoirxz <khoirrurrohman@gmail.com>
2025-04-27 16:36:18 +0700

    drawer

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -1,5 +1,8 @@
+import { useLocation } from "react-router";
+
 import { items } from "@/const/menuItems";
 import cardLogo from "../assets/card.svg";
+import { LinkItem } from "./Sidebar";
 
 export default function Drawer({
   drawerOpen,
@@ -9,6 +12,8 @@ export default function Drawer({
   drawerOpen: boolean;
   setDrawerOpen: (open: boolean) => void;
 }) {
+  const { pathname } = useLocation();
+
   return (
     <>
       {/* Overlay */}
@@ -37,18 +42,7 @@ export default function Drawer({
         {/* Content */}
         <div className="flex flex-col gap-2 mt-6">
           {items.map((item) => (
-            <button
-              key={item.name}
-              className={`flex items-center gap-5 w-full relative py-3 px-8 cursor-pointer hover:bg-[#2D60FF]/10 ${
-                item.active ? "text-[#2D60FF]" : "text-[#b1b1b1]"
-              }`}>
-              <span
-                className={`absolute left-0 w-1 h-full rounded-br-lg rounded-tr-lg ${
-                  item.active ? "bg-[#2D60FF]" : "bg-transparent"
-                }`}></span>
-              <item.icon fill={item.active ? "#2D60FF" : "#b1b1b1"} />
-              <span className="text-lg font-medium">{item.name}</span>
-            </button>
+            <LinkItem key={item.name} item={item} current={pathname} />
           ))}
         </div>
       </div>
