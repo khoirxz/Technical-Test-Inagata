b44b2d26bb187f8e46846b34a556da2d474af41c
khoirxz <khoirrurrohman@gmail.com>
2025-04-27 16:15:23 +0700

    fix typo format image :p

--- src/const/person.ts
+++ src/const/person.ts
@@ -6,31 +6,31 @@ export const personData: {
 }[] = [
   {
     id: 1,
-    image: "/person1.jpg",
+    image: "/person1.JPG",
     name: "Livia Bator",
     position: "CEO",
   },
   {
     id: 2,
-    image: "/person2.jpg",
+    image: "/person2.JPG",
     name: "Randy Press",
     position: "Director",
   },
   {
     id: 3,
-    image: "/person3.jpg",
+    image: "/person3.JPG",
     name: "Workman",
     position: "Designer",
   },
   {
     id: 4,
-    image: "/person3.jpg",
+    image: "/person3.JPG",
     name: "Workman",
     position: "Designer",
   },
   {
     id: 5,
-    image: "/person3.jpg",
+    image: "/person3.JPG",
     name: "Workman",
     position: "Designer",
   },
